af804b1b10cc0194ad0fc0422d29fd69b08ef606
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-05-09 12:47:27 +0530

    Updated Solution - GfG to GitHub

---
+++ Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {     unordered_map<int, int> prefixSumMap; // Map to store prefix sum
+        int prefixSum = 0;
+        int maxLength = 0;
+        for(int i = 0; i < n; i++){
+            prefixSum += A[i];
+            if(prefixSum == 0) {
+                maxLength = i + 1;
+            } else if(prefixSumMap.find(prefixSum) != prefixSumMap.end()) {
+                maxLength = max(maxLength, i - prefixSumMap[prefixSum]);
+            } else {
+                prefixSumMap[prefixSum] = i;
+            }
+        }
+        return maxLength;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
